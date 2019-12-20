791d6bcd873cfa7d081bbaea37899fdc73a5552d
liuminglei <812934656@qq.com>
2019-12-20 11:41:54 +0800

    初始版本

---
+++ xmall-product/src/main/java/com/luas/cloud/XmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.luas.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmallProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmallProductApplication.class, args);
+	}
+
+}
