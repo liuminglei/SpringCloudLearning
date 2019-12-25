ad87c65d759f5d3085d5630ecc32ac561f0c2d4a
liuminglei <812934656@qq.com>
2019-12-25 11:24:35 +0800

    四：服务消费者（feign）

---
+++ 04/xmall-product-clients-feign/src/main/java/com/luas/xmall/product/clients/XmallProductClientsFeignApplication.java
@@ -0,0 +1,15 @@
+package com.luas.xmall.product.clients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients("com.luas.xmall.product.clients.clients")
+@SpringBootApplication
+public class XmallProductClientsFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmallProductClientsFeignApplication.class, args);
+    }
+
+}
