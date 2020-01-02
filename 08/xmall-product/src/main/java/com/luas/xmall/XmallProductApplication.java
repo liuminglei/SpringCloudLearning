45a882015004ac6780a9956f31c9d8219c95804d
liuminglei <812934656@qq.com>
2020-01-02 11:42:48 +0800

    八：授权中心（Spring Cloud Oauth2） 修改相关配置 新增nacos发现及配置

--- 08/xmall-product/src/main/java/com/luas/xmall/XmallProductApplication.java
+++ 08/xmall-product/src/main/java/com/luas/xmall/XmallProductApplication.java
@@ -2,7 +2,9 @@ package com.luas.xmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
+@EnableResourceServer
 @SpringBootApplication
 public class XmallProductApplication {
 
